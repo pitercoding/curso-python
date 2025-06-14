51de77efcdd8e962dece601a3951e9aec4dbcc1c
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-06-14 09:13:25 +0200

    desafio 10 feito

---
+++ exercicios/ex010.py
@@ -0,0 +1,4 @@
+print('==== Conversor Monetário ====')
+reais = float(input('Quantos R$ você deseja converter? '))
+dolar = reais / 3.27
+print('Convertendo {:.2f}R$, você terá {:.2f}US$'.format(reais, dolar))
