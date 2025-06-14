369acbe2fcc149d7e8808c5cd82f2fddcc4ac6b2
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-06-14 15:22:59 +0200

    .

--- exercicios/ex005.py
+++ exercicios/ex005.py
@@ -1,4 +1,4 @@
 n = int (input('Digite um número: '))
 ant = n - 1
 suc = n + 1
-print('O número escolhido foi {} \n O antecessor é {} \n O sucessor é {}'.format(n, ant, suc))
+print('O número escolhido foi {} \nO antecessor é {} \nO sucessor é {}'.format(n, ant, suc))
