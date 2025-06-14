babbc486ce685fa6cf172627183fc4fbd98e098b
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-06-14 20:56:10 +0200

    ajustes desafio 10

--- exercicios/ex010.py
+++ exercicios/ex010.py
@@ -1,4 +1,7 @@
 print('==== Conversor Monetário ====')
-reais = float(input('Quantos R$ você deseja converter? '))
-dolar = reais / 3.27
-print('Convertendo {:.2f}R$, você terá {:.2f}US$'.format(reais, dolar))
+reais = float(input('Quantos R$ você deseja converter? R$'))
+dolar = reais / 5.54
+euro = reais / 6.41
+yuan = reais / 0.77
+print('==== Resultado ====')
+print('Convertendo {:.2f}R$, você terá: \nDólar: {:.2f}US$ \nEuro: {:.2f}€ \nYuan: {:.2f}¥'.format(reais, dolar, euro, yuan))
