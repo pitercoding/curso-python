c56133584d94f08da3927ff0fe1a049a84529825
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-06-19 16:47:05 +0200

    Aula 13 - Estrutura de repetição for

---
+++ aulas/aula013.py
@@ -0,0 +1,21 @@
+'''for c in range(0, 7, 2):
+    print(c)
+print('FIM')'''
+
+'''n = int(input('Digite um numero: '))
+for c in range (0, n+1, 5):
+    print(c)
+print('FIM')'''
+
+'''i = int(input('Início: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range (i, f+1, p):
+    print(c)
+print('FIM')'''
+
+s = 0
+for c in range (0, 4):
+    n = int(input('Digite um numero: '))
+    s += c
+print(f'A soma dos {n} números digitados é {s}.')
