d5ae6ad03d9dcbfb39fcf193b1ebc9c798a2d6b9
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-06-25 21:48:17 +0200

    Aula 17 - Listas (Parte 1)

---
+++ aulas/aula017.py
@@ -0,0 +1,20 @@
+num = [2, 5, 9 , 1]
+num[2] = 3
+num.append(7)
+num.sort(reverse=True)
+num.insert(2, 2)
+if 5 in num:
+    num.remove(5)
+else:
+    print('Não achei o número 4')
+print(num)
+print(f'Essa lista tem {len(num)} elementos.')
+
+print('-'*30)
+
+valores = list()
+for cont in range(0,5):
+    valores.append(int(input('Digite um valor: ')))
+for c, v in enumerate(valores):
+    print(f'Na posição {c} encontrei o valor {v}!')
+print('FIM')
