1b152239c8bd0a418693cb346c12ca298f697e79
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-06-29 20:14:20 +0200

    Desafio 97 - Um print especial

---
+++ exercicios/ex097.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~'*tam)
+    print(f'  {msg}')
+    print('~'*tam)
+
+
+#Programa Principal
+escreva('Piter Gomes')
+escreva('Flamengo Campeão do mundial de clubes!')
+escreva('Ouviram do Ipiranga as margens plácidas. De um povo heróico o brado retumbante...')
