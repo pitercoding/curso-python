d45c69cba6328595ce2c628914fbed7e885c292e
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-07-02 20:53:44 +0200

    Aula 23 - Tratamento de Erros e Exceções

---
+++ aulas/aula023.py
@@ -0,0 +1,18 @@
+try:
+    a = int(input('Numerador: '))
+    b = int(input('Denominador: '))
+    r = a / b
+#except Exception as erro:
+    #print(f'O erro foi {erro.__class__}')
+except (ValueError, TypeError):
+    print('Tivemos um problema com os tipos de dados informados.')
+except ZeroDivisionError:
+    print('Não é possível dividir um número por zero.')
+except KeyboardInterrupt:
+    print('O usuário preferiu não informar os dados!')
+except Exception as erro:
+    print(f'O erro encontrado foi {erro.__cause__}')
+else:
+    print(f'O resultado é {r:.1f}')
+finally:
+    print('Volte sempre! Muito obrigado.')
