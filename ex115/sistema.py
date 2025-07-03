fa1ddf73713f28905b7adfbdca79878c7a619388
Piter Gomes <142267146+pitercoding@users.noreply.github.com>
2025-07-03 14:43:24 +0200

    Desafio 115 b e c - Finalizando o projeto

--- ex115/sistema.py
+++ ex115/sistema.py
@@ -1,11 +1,22 @@
 from ex115.lib.interface import *
+from ex115.lib.arquivo import *
 from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
 while True:
-    resposta = menu(['Criar Arquivo', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
     if resposta == 1:
-        cabeçalho('\033[32mArquivo\033[m')
+        #Opção de listar o conteúdo de um arquivo!
+        lerArquivo(arq)
     elif resposta == 2:
-        cabeçalho('\033[32mPessoa\033[m')
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
         cabeçalho('Saindo do sistema... Até logo!')
         break
